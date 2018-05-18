6e9bba53c2803e2f89381700c496b941b6817fbe
grzegorz92 <grzegorz.jakubiec3@gmail.com>
2018-05-18 16:03:14 +0200

    Added http request service

---
+++ src/app/warning_message/requests.service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+
+@Injectable()
+export class RequestService{
+
+  // constructor(private http: Http){}
+  //
+  // storeServers(servers: any[]){
+  //
+  //   this.http.post( ,servers);
+  // }
+
+}
